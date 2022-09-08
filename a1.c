a0ca04ba251b33e2391c3aca3ef7aa61e1f03906
anajuliasouza03 <anajuliapereiradesouza003@gmail.com>
2022-09-07 20:42:37 -0400

    arrumando aula 1

--- a1.c
+++ a1.c
@@ -1,5 +1,19 @@
-main(){
-    printf("Olá, mundo!");
+#include<stdio.h>
 
+int main(){
+    int dia, mes, ano;
+    char nome[10];
+
+    printf("entre com o dia:\n>> ");
+    scanf("%d", &dia);
+    printf("entre com o mes: \n>>");
+    scanf("%d",&mes);
+    printf("o dia e o mes digitados foram: %d %d ", dia, mes);
+
+    printf("\nn entre com o nome: ");
+    scanf("%s", &nome);
+    printf("o nome digitado foi: %s", nome);
+
+    return 0;
 
 }
