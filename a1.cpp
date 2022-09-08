3b102b71d3e6b62958b73af3db71d0d43bb83073
anajuliasouza03 <anajuliapereiradesouza003@gmail.com>
2022-09-07 20:11:56 -0400

    aula 1

---
+++ a1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+    int dia, mes, ano;
+    char nome[10];
+
+    printf("entre com o dia:\n>> ");
+    scanf("%d", &dia);
+    printf("entre com o mes: \n>>");
+    scanf("%d",&mes);
+    printf("o dia e o mes digitados foram: %d %d ", dia, mes);
+
+    printf("\nn entre com o nome: ");
+    scanf("%s", &nome);
+    printf("o nome digitado foi: %s", nome);
+
+}
